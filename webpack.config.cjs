4d81c3d0b4899273d21c0be461b309d120aae1bb
daizhilian <daizhilian@gf.com.cn>
2021-09-08 12:13:15 +0800

    refactor: 相对路径修改

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,56 +1,57 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  devtool: 'eval-cheap-module-source-map',
+  mode: "production",
+  entry: "./src/index.js",
+  devtool: "eval-cheap-module-source-map",
   devServer: {
     static: [
       {
-        directory: path.join(__dirname, 'data'),
-        publicPath: '/data',
+        directory: path.join(__dirname, "data"),
+        publicPath: "/data",
       },
       {
-        directory: path.join(__dirname, 'public'),
-        publicPath: '/public',
+        directory: path.join(__dirname, "public"),
+        publicPath: "/public",
       },
       {
-        directory: path.join(__dirname, 'view'),
-        publicPath: '/view',
+        directory: path.join(__dirname, "docs"),
+        publicPath: "/docs",
       },
       {
-        directory: path.join(__dirname, 'src'),
-        publicPath: '/src',
+        directory: path.join(__dirname, "src"),
+        publicPath: "/src",
       },
     ],
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-    clean: true,
+    path: path.resolve(__dirname, "docs"),
+    filename: "bundle.js",
   },
   // resolve: {
   //   extensions: ['.js'],
   // },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                "targets": "> 0.25%, not dead",
-                "useBuiltIns": "usage",
-                "corejs": "3.8.3",
-              }
-            ]
-          ]
-        }
-      }
-    }]
-  }
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: "> 0.25%, not dead",
+                  useBuiltIns: "usage",
+                  corejs: "3.8.3",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
